2ec653570b792439846489b16f7e0805c27fa95a
Amirul Faqih <fakiiahmad001@gmail.com>
2024-07-04 21:48:01 +0300

    chore: Update portfolio description with correct name

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Portfolio of Fakii Mohammed - Software Developer",
   description:
-    "This is the portfolio of Abu Said. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+    "This is the portfolio of Fakii Mohammed. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
 };
 
 // Define the layout component 
