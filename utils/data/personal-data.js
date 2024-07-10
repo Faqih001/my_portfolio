68acfd80ff9e83490eeebf4fd9008b854e67e18a
Amirul Faqih <fakiiahmad001@gmail.com>
2024-07-10 18:34:32 +0300

    chore: Update personal data with correct designation

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,7 +1,7 @@
 export const personalData = {
   name: "Fakii Mohammed",
   profile: '/profile.jpeg',
-  designation: "Software Developer",
+  designation: "Software Engineer",
   description: "My name is Fakii Mohammed. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'fakiiahmad001@gmail.com',
   phone: '+254 741140250',
