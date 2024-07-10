a7f4e06959ae0e0596e9ac400fb7bb2e6b3c64f8
Amirul Faqih <fakiiahmad001@gmail.com>
2024-07-10 19:56:02 +0300

    refactor: Update experience data in experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,44 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Software Engineer I',
-    company: "Teton Private Ltd.",
-    duration: "(Jan 2022 - Present)"
+    title: 'IT Technician',
+    company: "MOWASSCO",
+    duration: "(Sep 2021 - Dec 2021)"
   },
   {
     id: 2,
     title: "FullStack Developer",
     company: "Fiverr (freelance)",
-    duration: "(Jun 2021 - Jan 2022)"
+    duration: "(Jan 2022 - Dec 2022)"
   },
   {
     id: 3,
-    title: "Self Employed",
-    company: "Code and build something in everyday.",
-    duration: "(Jan 2018 - Present)"
+    title: "Trainer For Artificial Intelligence Bootcamp",
+    company: "Kabarak University",
+    duration: "(Jan 2023 - April 2023)"
+  },
+  {
+    id: 4,
+    title: "Trainee for Introduction to Innovation",
+    company: "Kabarak University",
+    duration: "(Jun 2023 - Aug 2023)"
+  },
+  {
+    id: 5,
+    title: "Innovator At Central Innovation Week",
+    company: "Kabarak University",
+    duration: "(Sept 2023 - Sep 2023)"
+  },
+  {
+    id: 6,
+    title: "Innovator At Kenya Innovation Week",
+    company: "Kenya Innovation Agency",
+    duration: "(Nov 2023 - Nov 2023)"
+  },
+  {
+    id: 7,
+    title: "ICT Support Technician",
+    company: "Health IT",
+    duration: "(Feb 2024 - Present)"
   }
 ]
