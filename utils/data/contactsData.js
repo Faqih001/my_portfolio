939e69a490309d2786efcfae5d8ee9bbc74e8d61
Amirul Faqih <fakiiahmad001@gmail.com>
2024-07-10 19:14:36 +0300

    chore: Update Facebook profile link in contactsData.js

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -3,7 +3,7 @@ export const contactsData = {
     phone: '+254741140250',
     address: 'Kenya, Nakuru Kabarak - 20157',
     github: 'https://github.com/Faqih001',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
+    facebook: 'https://www.facebook.com/amirul.faqih/',
     linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
     twitter: 'https://twitter.com/said7388',
     stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
