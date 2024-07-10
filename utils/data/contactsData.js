16f04cc2bc50a1b3f9e3385867388e6d469c373a
Amirul Faqih <fakiiahmad001@gmail.com>
2024-07-10 19:14:13 +0300

    chore: Update GitHub profile link in personal data

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -2,7 +2,7 @@ export const contactsData = {
     email: 'fakiiahmad001@gmail.com',
     phone: '+254741140250',
     address: 'Kenya, Nakuru Kabarak - 20157',
-    github: 'https://github.com/said7388',
+    github: 'https://github.com/Faqih001',
     facebook: 'https://www.facebook.com/abusaid.riyaz/',
     linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
     twitter: 'https://twitter.com/said7388',
