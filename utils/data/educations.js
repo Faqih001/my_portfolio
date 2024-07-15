6834a2c62379bfbb51a3d337fa02a892a6a057ca
Amirul Faqih <fakiiahmad001@gmail.com>
2024-07-15 11:45:43 +0300

    refactor: Update educations data with new degree and institution

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,18 +1,24 @@
 export const educations = [
   {
     id: 1,
+    title: "Masters Degree in Information Technology",
+    duration: "2022 - To Date",
+    institution: "Kabarak University, Nakuru - Kenya",
+  },
+  {
+    id: 2,
     title: "Bachelor Degree in Information Technology",
     duration: "2018 - 2021",
     institution: "Kabarak University, Nakuru - Kenya",
   },
   {
-    id: 2,
+    id: 3,
     title: "Kenya Certificate of Secondary Education",
     duration: "2014 - 2017",
     institution: "Moi Forces Academy - Mombasa",
   },
   {
-    id: 3,
+    id: 4,
     title: "Kenya Certificate of Primary Education",
     duration: "2006 - 2013",
     institution: "Kenya Navy Primary School",
