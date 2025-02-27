ca3b20345745b528b1953f94864789ff041262c4
Fakii Mohammed <fakiiahmad001@gmail.com>
2025-02-27 13:47:35 +0300

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,44 +1,44 @@
 export const experiences = [
   {
     id: 1,
-    title: 'IT Technician',
+    title: "IT Technician",
     company: "MOWASSCO",
     duration: "(Sep 2021 - Dec 2021)"
   },
   {
     id: 2,
     title: "FullStack Developer",
-    company: "Fiverr (freelance)",
+    company: "Fiverr (Freelance)",
     duration: "(Jan 2022 - Dec 2022)"
   },
   {
     id: 3,
-    title: "Trainer For Artificial Intelligence Bootcamp",
+    title: "Trainer - AI Bootcamp",
     company: "Kabarak University",
     duration: "(Jan 2023 - April 2023)"
   },
   {
     id: 4,
-    title: "Trainee for Introduction to Innovation",
+    title: "Trainee - Introduction to Innovation",
     company: "Kabarak University",
     duration: "(Jun 2023 - Aug 2023)"
   },
   {
     id: 5,
-    title: "Innovator At Central Innovation Week",
+    title: "Innovator - Central Rift Innovation Week (CRIW)",
     company: "Kabarak University",
     duration: "(Sept 2023 - Sep 2023)"
   },
   {
     id: 6,
-    title: "Innovator At Kenya Innovation Week",
+    title: "Innovator - Kenya Innovation Week (KIW)",
     company: "Kenya Innovation Agency",
     duration: "(Nov 2023 - Nov 2023)"
   },
   {
     id: 7,
-    title: "ICT Support Technician",
-    company: "Health IT",
+    title: "Software Engineer",
+    company: "USAID Digital Health Ecosystem Project",
     duration: "(Feb 2024 - Present)"
   }
-]
+];
